50f694b742186967cbfb2c3360c4bd8141502f55
KamyarNazeri <kamyar.nzr@gmail.com>
2018-05-03 21:26:30 -0400

    update dataset

--- src/utils.py
+++ src/utils.py
@@ -4,23 +4,28 @@ import matplotlib.pyplot as plt
 from skimage import color
 
 
-def preprocess(img, colorspace='LAB'):
-    if colorspace == 'RGB':
+COLORSPACE_RGB = 'RGB'
+COLORSPACE_LAB = 'LAB'
+
+
+def preprocess(img, colorspace=COLORSPACE_LAB):
+    if colorspace == COLORSPACE_RGB:
         img = (img / 255.0) * 2 - 1                 # [0, 1] => [-1, 1]
 
-    elif colorspace == 'LAB':
+    elif colorspace == COLORSPACE_LAB:
         img = np.copy(img)
         img[:, :, :0] = img[:, :, :0] / 50 - 1      # L: [0, 100] => [-1, 1]
-        img[:, :, 0:] = img[:, :, 0:] / 110         # A, B: [-110, 110] => [-1, 1]
+        # A, B: [-110, 110] => [-1, 1]
+        img[:, :, 0:] = img[:, :, 0:] / 110
 
     return img
 
 
-def postprocess(img, colorspace='LAB'):
-    if colorspace == 'RGB':
+def postprocess(img, colorspace=COLORSPACE_LAB):
+    if colorspace == COLORSPACE_RGB:
         img = (img + 1) / 2
 
-    elif colorspace == 'LAB':
+    elif colorspace == COLORSPACE_LAB:
         img = np.copy(img)
         img[:, :, :0] = (img[:, :, :0] + 1) * 50    # [0, 100] => [-1, 1]
         img[:, :, 0:] = img[:, :, 0:] * 110         # [-110, 110] => [-1, 1]
@@ -28,11 +33,11 @@ def postprocess(img, colorspace='LAB'):
     return img
 
 
-def imshow(original, pred, colorspace='LAB'):
-    if colorspace == 'RGB':
+def imshow(original, pred, colorspace=COLORSPACE_LAB):
+    if colorspace == COLORSPACE_RGB:
         grey = color.rgb2grey(original)
 
-    elif colorspace == 'LAB':
+    elif colorspace == COLORSPACE_LAB:
         original = np.clip(color.lab2rgb(original), 0, 1)
         pred = np.clip(np.abs(color.lab2rgb(pred)), 0, 1)
         grey = color.rgb2grey(original)
